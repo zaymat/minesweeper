ff1feedd2a1688ddd99b2a7642e42c391c0e3354
zaymat <may-26@hotmail.fr>
2018-05-18 16:15:28 +0200

    Improve colors

--- minesweeper.js
+++ minesweeper.js
@@ -65,10 +65,16 @@ const displayGrid = (grid) => {
             }else if(cell[0] == "flagged"){
                 html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:handleRightClick(this);return false;\" class=\"flagged\"><img src=\"flag.png\" class=\"image\"></img></td>"
             }else{
-                if(cell[1] == -1){
-                    html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:return false;\" class=\"discovered\"><img src=\"bomb.png\" class=\"image\"></img></td>"
-                }else{
-                    html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:handleRightClick(this);return false;\" onclick=\"handleLeftClick(this)\" class=\"discovered\">"+ cell[1] + "</td>"
+                switch(cell[1]){
+                    case -1:
+                        html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:return false;\" class=\"discovered\"><img src=\"bomb.png\" class=\"image\"></img></td>"
+                        break;
+                    case 0:
+                        html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:return false;\" class=\"discovered number0\"></td>"
+                        break;
+                    default:
+                        html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:return false;\" class=\"discovered number" + cell[1] + "\">" + cell[1] + "</td>"
+                        break;
                 }
             }
         }
@@ -94,10 +100,16 @@ const displayStaticGrid = (grid) => {
             }else if(cell[0] == "flagged"){
                 html += "<td oncontextmenu=\"javascript:return false;\" class=\"flagged\"><img src=\"flag.png\" class=\"image\"></img></td>"
             }else{
-                if(cell[1] == -1){
-                    html += "<td oncontextmenu=\"javascript:return false;\" class=\"discovered\"><img src=\"bomb.png\" class=\"image\"></img></td>"
-                }else{
-                    html += "<td id=\'{" + "\"x\":" + i + ", \"y\":" + j + "}\' oncontextmenu=\"javascript:return false;\" class=\"discovered\">"+ cell[1] + "</td>"
+                switch(cell[1]){
+                    case -1:
+                        html += "<td oncontextmenu=\"javascript:return false;\" class=\"discovered\"><img src=\"bomb.png\" class=\"image\"></img></td>"
+                        break;
+                    case 0:
+                        html += "<td oncontextmenu=\"javascript:return false;\" class=\"discovered number0\"></td>"
+                        break;
+                    default:
+                        html += "<td oncontextmenu=\"javascript:return false;\" class=\"discovered number" + cell[1] + "\">" + cell[1] + "</td>"
+                        break;
                 }
             }
         }
@@ -126,23 +138,22 @@ const handleLeftClick = (e) => {
     var x = coor["x"];
     var y = coor["y"];
 
-    if(grid["grid"][x][y][0] != "flagged"){
-        grid["grid"][x][y][0] = "discovered";
-        grid["hidden_cells"] -= 1;
-        if(grid["grid"][x][y][1] == 0){
-            discover(grid, x, y);
-            displayGrid(grid["grid"]);
-        }else if(grid["grid"][x][y][1] == -1){
-            var loose = document.getElementById("score");
-            loose.innerHTML = "You loose !";
-            displayStaticGrid(grid["grid"]);
-        }else if(checkWin(grid)){
-            var win = document.getElementById("score");
-            win.innerHTML = "You Win !";
-            displayStaticGrid(grid["grid"]);
-        }else{
-            displayGrid(grid["grid"]);
-        }
+    grid["grid"][x][y][0] = "discovered";
+    grid["hidden_cells"] -= 1;
+
+    if(grid["grid"][x][y][1] == 0){
+        discover(grid, x, y);
+        displayGrid(grid["grid"]);
+    }else if(grid["grid"][x][y][1] == -1){
+        var loose = document.getElementById("score");
+        loose.innerHTML = "You loose !";
+        displayStaticGrid(grid["grid"]);
+    }else if(checkWin(grid)){
+        var win = document.getElementById("score");
+        win.innerHTML = "You Win !";
+        displayStaticGrid(grid["grid"]);
+    }else{
+        displayGrid(grid["grid"]);
     }
 }
 
